03d01b5b198624cb4326ac077cac95619808b8b6
Harshita222 <kumariharsita501@gmail.com>
2024-06-20 22:28:31 +0530

    'Done'

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const handleClick = () => {
+    console.log("12");
+    setCounter(counter + 1);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <p> Button clicked {counter} times</p>
+      <button onClick={handleClick}>Click me</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
