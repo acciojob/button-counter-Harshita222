44496ac6abb2e9a5275358ac2c11e1e0d26e95d3
Harshita222 <kumariharsita501@gmail.com>
2024-06-21 12:41:02 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,36 @@
+// import React, { useState } from "react";
+// import "./../styles/App.css";
+
+// const App = () => {
+//   const [counter, setCounter] = useState(0);
+
+//   const handleClick = () => {
+//     setCounter(counter + 1);
+//   };
+
+//   return (
+//     <div>
+//       {/* Do not remove the main div */}
+//       <p> Button clicked {counter} times</p>
+//       <button onClick={handleClick}>Click me</button>
+//     </div>
+//   );
+// };
+
+// export default App;
+
 import React, { useState } from "react";
 import "./../styles/App.css";
-
 const App = () => {
-  const [counter, setCounter] = useState(0);
-
-  const handleClick = () => {
-    // console.log("12");
-    setCounter(counter + 1);
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
   };
-
   return (
     <div>
-      {/* Do not remove the main div */}
-      <p> Button clicked {counter} times</p>
-      <button onClick={handleClick}>Click me</button>
+      <p>Button clicked {count} times</p>
+      <button onClick={increment}>Click me</button>
     </div>
   );
 };
-
 export default App;
